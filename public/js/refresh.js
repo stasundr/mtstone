d913ff1a1c34b5219d4f1ec67d687fe89dc72503
Stanislav Dryomov <stasundr@gmail.com>
2016-04-23 07:59:33 +0600

    refresh.js cleaning

--- public/js/refresh.js
+++ public/js/refresh.js
@@ -1,29 +1,23 @@
 'use strict';
 
-function showPreview(file) {
-    //$('#preview').replaceWith('<div id=\"preview\"><img src="' + window.location.href.replace(/#$/, '')  + file + '.png"/></div>')
-
-    var img = `<div id="preview"><img src="${window.location.href.replace(/#$/, '')}${file}.png"/></div>`;
-    $('#preview').replaceWith(img);
+function preview(file) {
+    $('#preview').attr('src', `${window.location.pathname}${file}.png`);
 }
 
 $(
     setInterval(function() {
         $.post('/refresh', function(data) {
             var list = '';
-            console.log(data);
             data.originalNames.forEach(function(filename, index) {
-                //list += '<a href="#" onclick="showPreview(\'' + data.publicNames[index] + '\')">' + filename + '</a>';
-
                 if (data.fileStatus[index])
-                    list += '<a href="#" onclick="showPreview(\'' + data.publicNames[index] + '\')">' + filename + '</a> <i class="fa fa-check"></i>';
+                    list += `<a href="#" onclick="preview('${data.publicNames[index]}')">${filename}</a> <i class="fa fa-check"></i>`;
                 else
-                    list += filename + ' <i class="fa fa-hourglass"></i>';
+                    list += `${filename} <i class="fa fa-hourglass"></i>`;
 
                 list += '<br/>';
             });
 
-            $('#list').replaceWith('<div id="list">' + list + '</div>');
+            $('#list').html(list);
         })
     }, 1000)
 );
